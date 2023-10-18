56635c549f46f31243c2bdb1db9c011035722236
Roman <Chepkinrv@mail.ru>
2023-10-18 22:34:28 +0700

    класс GenreYear для получения всех жанров и годов класс FilterMoviesView для сортировки

--- mainapp/movies/views.py
+++ mainapp/movies/views.py
@@ -1,16 +1,36 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView, DetailView
 
-from movies.models import Movie
+from movies.models import Movie, Genre
 
 
-class MoviesView(ListView) :
+class GenreYear():
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MoviesView(ListView, GenreYear) :
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     context_object_name = 'movies'
 
 
-class MovieDetailView(DetailView) :
+class MovieDetailView(DetailView, GenreYear) :
     model = Movie
     slug_field = 'url'
+
+
+class FilterMoviesView(ListView, GenreYear):
+    context_object_name = 'movies'
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        )
+        return queryset
