bcfdaa8fbca07fa82030d0ced75f2c23a77e0537
Roman <Chepkinrv@mail.ru>
2023-10-18 16:30:30 +0700

    добавил url

--- mainapp/movies/urls.py
+++ mainapp/movies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from movies.views import MoviesView
+from movies.views import MoviesView, MovieDetailView
 
 app_name = 'movies'
 
 urlpatterns = [
-    path('', MoviesView.as_view())
+    path('', MoviesView.as_view()),
+    path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail')
 ]
