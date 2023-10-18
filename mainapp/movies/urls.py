44d4371c9bab2a9b92207bfd1c15e4f529dc815a
Roman <Chepkinrv@mail.ru>
2023-10-18 22:34:41 +0700

    url для сортировки

--- mainapp/movies/urls.py
+++ mainapp/movies/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from movies.views import MoviesView, MovieDetailView
+from movies.views import MoviesView, MovieDetailView, FilterMoviesView
 
 app_name = 'movies'
 
 urlpatterns = [
     path('', MoviesView.as_view()),
+    path('filter/', FilterMoviesView.as_view(), name='filter'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail')
+
 ]
