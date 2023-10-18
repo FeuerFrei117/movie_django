d941a6345b7684c140051c7d6d3304753cf9aeea
Roman <Chepkinrv@mail.ru>
2023-10-18 14:39:59 +0700

    добавил контроллер

--- mainapp/movies/views.py
+++ mainapp/movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from movies.models import Movie
+
+
+class MoviesView(View) :
+
+    def get(self, request) :
+        movies = Movie.objects.all()
+        return render(request, 'movies/movies.html', {'movies_list' : movies})
