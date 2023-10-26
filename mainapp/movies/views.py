b080f50d5c56e743672683053131c753735b2f57
Roman <Chepkinrv@mail.ru>
2023-10-26 22:52:46 +0700

    добавил кадры к фильмам вывод комментариев добавление комментариев

--- mainapp/movies/views.py
+++ mainapp/movies/views.py
@@ -1,14 +1,17 @@
 from django.db.models import Q
+from django.shortcuts import redirect
+from django.views import View
 from django.views.generic import ListView, DetailView
 
 from movies.models import Movie, Genre
+from movies.forms import ReviewForm
 
 
-class GenreYear():
-    def get_genres(self):
+class GenreYear() :
+    def get_genres(self) :
         return Genre.objects.all()
 
-    def get_years(self):
+    def get_years(self) :
         return Movie.objects.filter(draft=False).values('year')
 
 
@@ -23,10 +26,21 @@ class MovieDetailView(DetailView, GenreYear) :
     slug_field = 'url'
 
 
-class FilterMoviesView(ListView, GenreYear):
+class AddReview(View):
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
+
+
+class FilterMoviesView(ListView, GenreYear) :
     context_object_name = 'movies'
 
-    def get_queryset(self):
+    def get_queryset(self) :
         queryset = Movie.objects.filter(
             Q(year__in=self.request.GET.getlist('year')) |
             Q(genres__in=self.request.GET.getlist('genre'))
